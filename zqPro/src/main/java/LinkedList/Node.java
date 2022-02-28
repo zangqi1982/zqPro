b153747c6d7714e3d605e64167fd048a8ef462d3
coffee-and-tea <40965619+coffee-and-tea@users.noreply.github.com>
2022-02-28 10:40:39 -0600

    Update Node.java
    update Node to standard format

--- zqPro/src/main/java/LinkedList/Node.java
+++ zqPro/src/main/java/LinkedList/Node.java
@@ -1,9 +1,27 @@
 package LinkedList;
 
 public class Node {
-    int data;
-    public Node next=null;
-    public Node(int data){
-        this.data=data;
+    
+    private int data;
+    private Node next;
+    
+    public Node() {}
+    
+    public Node(int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+    
+    public int getData() { return data; }
+    public Node getNext() { return next; }
+    
+    public int setData(int data) { this.data = data; }
+    public Node setNext(Node next) { this.next = next; }
+    
+    public static void main(String [] args) {
+        Node n = new Node();
+        System.out.println(n.getData()); // 0
+        System.out.println(n.getNext()); // null  
+//         n.setNext(...)
     }
 }
