724ee1572df867fea4a8ce51a3e4f5bbc6ad7080
coffee-and-tea <40965619+coffee-and-tea@users.noreply.github.com>
2022-02-28 10:17:38 -0600

    Update LOA.java
    change use object instead of static

--- zqPro/src/main/java/LargestOfArray/LOA.java
+++ zqPro/src/main/java/LargestOfArray/LOA.java
@@ -5,7 +5,7 @@ public class LOA {
 
 //    static int[] array = {1, 2, 3, 4, 5, 4, 3, 2, -1};
 //    static int[] array = { -1};
-    static int[] array = { };
+//     static int[] array = { };
 
     // input is null
     // input is empty
@@ -15,7 +15,21 @@ public class LOA {
     public static void main(String[] Args) {
         int result = findLargestValue();
         System.out.println(result);
-    }
+        
+        
+        int [] testArray = null;
+        int [] testArray2 = new int[0];
+        int [] testArray3 = new int[10];
+        // premative default 
+        for(int arrayItem: testArray3) {
+            System.out.println(arrayItem);
+        }
+        
+        LOA loa = new LOA();
+        loa.findLargestValue(testArray);
+        loa.findLargestValue(testArray2);
+        loa.findLargestValue(testArray3);
+     }
 
     /**
      * return largest value or
@@ -40,7 +54,22 @@ public class LOA {
     /**
      * return index of largest value, if same return last largest value's index
      * <p>
-     * when empty array return null, when array is null, return null
+     * when empty array return -1, when array is null, return -1
      */
+    
+    public int findLargestValue(int[] inputArray) {
+        if (inputArray == null) return -1;
+        if (inputArray.length == 0) return -1;
+            
+        int largestValue = Integer.MIN_VALUE;
+        int lastLargestIndex = -1;
+        for (int i = 0; i < inputArray.length; i++) {
+            if (largestValue <= inputArray[i]) {
+                largestValue = inputArray[i];
+                lastLargestIndex = i;
+            }
+        }
+        return lastLargestIndex;
+    }
 }
 
