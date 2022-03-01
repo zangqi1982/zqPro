7c42b7e2fbef6091bb9001040e15a8908c5b5e29
zangqi1982 <100374644+zangqi1982@users.noreply.github.com>
2022-03-01 13:10:06 -0600

    Add files via upload

--- zqPro/src/main/java/LinkedList/LinkedList.java
+++ zqPro/src/main/java/LinkedList/LinkedList.java
@@ -3,7 +3,7 @@ package LinkedList;
 public class LinkedList {
 
     // static means all linked list share this
-    public static Node head=null;
+    private static Node head= new Node(0);
     
     // count for how many linked list created
     public static int count = 0;
@@ -13,97 +13,97 @@ public class LinkedList {
     
     public static void main(String[] Args){
         boolean result;
-        appendNode(new Node(1));
-        appendNode(new Node(2));
-        appendNode(new Node(3));
-        appendNode(new Node(4));
-        appendNode(new Node(5));
-        showLinedList(head);
-        result=selectNode(3);
-        result=selectNode(0);
-        result=deleteNode(4);
-        result=deleteNode(0);
-        result=insertNode(new Node(33),3);
-        result=insertNode(new Node(44),4);
-        reversedLinkedList();
-        showLinedList(head);
+        Node head=new Node(0);
+        head.appendNode(head,new Node(2));
+        head.appendNode(head,new Node(3));
+        head.appendNode(head,new Node(4));
+        head.appendNode(head,new Node(5));
+        head.showLinedList(head);
+        result=head.selectNode(head,3);
+        result=head.selectNode(head,0);
+        result=head.deleteNode(head,4);
+        result=head.deleteNode(head,0);
+        result=head.insertNode(head,new Node(33),3);
+        result=head.insertNode(head,new Node(44),4);
+        head.reversedLinkedList(head);
+        head.showLinedList(head);
     }
-    public static void appendNode(Node targetNode){
-        Node currNode=head;
-        if(head==null){
-            head=targetNode;
-            showLinedList(head);
-        } else{
-            while(currNode.next!=null){
-                currNode=currNode.next;
-            }
-            currNode.next=targetNode;
-            showLinedList(head);
-        }
-    }
-    public static boolean insertNode(Node targetNode, int nodeValue) {
-        Node currNode=head;
-        while(currNode!=null){
-            if(currNode.data!=nodeValue){
-                currNode=currNode.next;
-            }else{
-                targetNode.next= currNode.next;
-                currNode.next=targetNode;
-                showLinedList(head);
-                return true;
-            }
-        }
-        System.out.println("Can's find specific value from the Linked List!");
-        return false;
-    }
-    public static boolean selectNode(int nodeValue){
-        Node currNode=head;
-        int i=0;
-        while(currNode!=null){
-            i++;
-            if(currNode.data!=nodeValue){
-                currNode=currNode.next;
-            }else{
-                System.out.println("The value node is in position: "+i);
-                return true;
-            }
-        }
-        System.out.println("Can's find specific value from the Linked List!");
-        return false;
-    }
-    public static boolean deleteNode(int nodeValue){
-        Node currNode=head;
-        Node prevNode=head;
-        while(currNode!=null){
-            if(currNode.data!=nodeValue){
-                prevNode=currNode;
-                currNode=currNode.next;
-            }else{
-                prevNode.next=currNode.next;
-                System.out.println("The value node is deleted successfully");
-                return true;
-            }
-        }
-        System.out.println("Can's find specific value from the Linked List!");
-        return false;
-    }
-    public static void reversedLinkedList(){
-        Node prevNode=null;
-        Node nextNode=null;
-        while(head.next!=null){
-            nextNode=head.next;
-            head.next=prevNode;
-            prevNode=head;
-            head=nextNode;
-        }
-        head.next=prevNode;
-    }
-    public static void showLinedList(Node headNode){
-        while(headNode!=null){
-            System.out.println("The LinkedList is: "+headNode.data+"->");
-            headNode=headNode.next;
-        }
-    }
+//    public static void appendNode(Node targetNode){
+//        Node currNode=head;
+//        if(head==null){
+//            head=targetNode;
+//            showLinedList(head);
+//        } else{
+//            while(currNode.next!=null){
+//                currNode=currNode.next;
+//            }
+//            currNode.next=targetNode;
+//            showLinedList(head);
+//        }
+//    }
+//    public static boolean insertNode(Node targetNode, int nodeValue) {
+//        Node currNode=head;
+//        while(currNode!=null){
+//            if(currNode.getData()!=nodeValue){
+//                currNode=currNode.next;
+//            }else{
+//                targetNode.next= currNode.next;
+//                currNode.next=targetNode;
+//                showLinedList(head);
+//                return true;
+//            }
+//        }
+//        System.out.println("Can's find specific value from the Linked List!");
+//        return false;
+//    }
+//    public static boolean selectNode(int nodeValue){
+//        Node currNode=head;
+//        int i=0;
+//        while(currNode!=null){
+//            i++;
+//            if(currNode.getData()!=nodeValue){
+//                currNode=currNode.next;
+//            }else{
+//                System.out.println("The value node is in position: "+i);
+//                return true;
+//            }
+//        }
+//        System.out.println("Can's find specific value from the Linked List!");
+//        return false;
+//    }
+//    public static boolean deleteNode(int nodeValue){
+//        Node currNode=head;
+//        Node prevNode=head;
+//        while(currNode!=null){
+//            if(currNode.getData()!=nodeValue){
+//                prevNode=currNode;
+//                currNode=currNode.next;
+//            }else{
+//                prevNode.next=currNode.next;
+//                System.out.println("The value node is deleted successfully");
+//                return true;
+//            }
+//        }
+//        System.out.println("Can's find specific value from the Linked List!");
+//        return false;
+//    }
+//    public static void reversedLinkedList(){
+//        Node prevNode=null;
+//        Node nextNode=null;
+//        while(head.next!=null){
+//            nextNode=head.next;
+//            head.next=prevNode;
+//            prevNode=head;
+//            head=nextNode;
+//        }
+//        head.next=prevNode;
+//    }
+//    public static void showLinedList(Node headNode){
+//        while(headNode!=null){
+//            System.out.println("The LinkedList is: "+headNode.getData()+"->");
+//            headNode=headNode.next;
+//        }
+//    }
 
 
 }
--- zqPro/src/main/java/LinkedList/Node.java
+++ zqPro/src/main/java/LinkedList/Node.java
@@ -2,26 +2,97 @@ package LinkedList;
 
 public class Node {
     
-    private int data;
-    private Node next;
+    public int data;
+    public Node next;
     
-    public Node() {}
-    
-    public Node(int data, Node next){
-        this.data = data;
-        this.next = next;
+
+    public Node(int dataValue){
+        this.data = dataValue;
+        this.next = null;
     }
     
     public int getData() { return data; }
     public Node getNext() { return next; }
     
-    public int setData(int data) { this.data = data; }
-    public Node setNext(Node next) { this.next = next; }
-    
-    public static void main(String [] args) {
-        Node n = new Node();
-        System.out.println(n.getData()); // 0
-        System.out.println(n.getNext()); // null  
-//         n.setNext(...)
+    public void setData(int dataValue) { this.data = dataValue; }
+    public void setNext(Node nextNode) { this.next = nextNode; }
+
+    void appendNode(Node head, Node targetNode){
+        Node currNode=head;
+        if(head==null){
+            head=targetNode;
+            showLinedList(head);
+        } else{
+            while(currNode.next!=null){
+                currNode=currNode.next;
+            }
+            currNode.next=targetNode;
+            showLinedList(head);
+        }
     }
+
+    boolean insertNode(Node head, Node targetNode, int nodeValue) {
+        Node currNode=head;
+        while(currNode!=null){
+            if(currNode.getData()!=nodeValue){
+                currNode=currNode.next;
+            }else{
+                targetNode.next= currNode.next;
+                currNode.next=targetNode;
+                showLinedList(head);
+                return true;
+            }
+        }
+        System.out.println("Can's find specific value from the Linked List!");
+        return false;
+    }
+    boolean selectNode(Node head, int nodeValue){
+        Node currNode=head;
+        int i=0;
+        while(currNode!=null){
+            i++;
+            if(currNode.getData()!=nodeValue){
+                currNode=currNode.next;
+            }else{
+                System.out.println("The value node is in position: "+i);
+                return true;
+            }
+        }
+        System.out.println("Can's find specific value from the Linked List!");
+        return false;
+    }
+    boolean deleteNode(Node head, int nodeValue){
+        Node currNode=head;
+        Node prevNode=head;
+        while(currNode!=null){
+            if(currNode.getData()!=nodeValue){
+                prevNode=currNode;
+                currNode=currNode.next;
+            }else{
+                prevNode.next=currNode.next;
+                System.out.println("The value node is deleted successfully");
+                return true;
+            }
+        }
+        System.out.println("Can's find specific value from the Linked List!");
+        return false;
+    }
+    void reversedLinkedList(Node head){
+        Node prevNode=null;
+        Node nextNode=null;
+        while(head.next!=null){
+            nextNode=head.next;
+            head.next=prevNode;
+            prevNode=head;
+            head=nextNode;
+        }
+        head.next=prevNode;
+    }
+    void showLinedList(Node headNode){
+        while(headNode!=null){
+            System.out.println("The LinkedList is: "+headNode.getData()+"->");
+            headNode=headNode.next;
+        }
+    }
+
 }
